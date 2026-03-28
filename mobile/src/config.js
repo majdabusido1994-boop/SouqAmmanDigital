5ae152f015ec2e063baafb012fa642ad502b7416
abusido94 <abusido94@gmail.com>
2026-03-29 00:05:00 +0400

    Update mobile config to point to Railway API

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -2,7 +2,7 @@
 // Update PRODUCTION_URL before deploying to app stores
 
 const DEV_URL = 'http://192.168.100.236:5000';
-const PRODUCTION_URL = 'https://souq-amman-digital-api.onrender.com';
+const PRODUCTION_URL = 'https://souq-amman-digital-api-production.up.railway.app';
 
 export const API_BASE = __DEV__ ? DEV_URL : PRODUCTION_URL;
 export const API_URL = `${API_BASE}/api`;
